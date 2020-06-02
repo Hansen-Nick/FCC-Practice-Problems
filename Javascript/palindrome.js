e28d05079b5322684461a6bc7c8031b48507a23f
Hansen-Nick <hansen.nick95@gmail.com>
2020-06-02 13:09:49 -0500

    Fixed accidentally deleted code

--- Javascript/palindrome.js
+++ Javascript/palindrome.js
@@ -1,1 +1,13 @@
+function palindrome(str) {
+  let strArray = str.split("");
+  let alphanum = strArray.filter((item) => item.match(/^[a-z0-9]+$/i));
+  let alphanumClone = [...alphanum];
+  console.log(alphanum);
+
+  return alphanumClone.join("").toLowerCase() ===
+    alphanum.reverse().join("").toLowerCase()
+    ? true
+    : false;
+}
+
 console.log(palindrome("A man, a plan, a canal. Panama"));
